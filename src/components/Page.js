88c5f769f861f5692f5cd4672384d2d25d5a7a2b
Taras Golotiuk <tarikmanos@gmail.com>
2017-05-09 20:42:41 +0300

    add css for "a"

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,19 +2,22 @@
  * Created by tarik on 09.05.2017.
  */
 import React, { PropTypes, Component } from 'react'
+import '../components/Page.css'
 
 export default class Page extends Component {
     onYearBtnClick(e) {
         this.props.setYear(+e.target.innerText)
     }
     render() {
-        const {year, photos} = this.props
-        console.log(this.props)
+        const {year, photos} = this.props;
+        console.log(this.props);
         return <div>
             <p>
-                <button onClick={::this.onYearBtnClick}>2016</button>
-                <button onClick={::this.onYearBtnClick}>2015</button>
-                <button onClick={::this.onYearBtnClick}>2014</button>
+                <a onClick={::this.onYearBtnClick}>2016</a>
+                <a onClick={::this.onYearBtnClick}>2015</a>
+                <a onClick={::this.onYearBtnClick}>2014</a>
+                <a onClick={::this.onYearBtnClick}>2013</a>
+                <a onClick={::this.onYearBtnClick}>2012</a>
             </p>
             <h3>{year} год</h3>
             <p>У тебя {photos.length} фото за {year} год</p>
@@ -25,4 +28,4 @@ Page.propTypes = {
     year: PropTypes.number.isRequired,
     photos: PropTypes.array.isRequired,
     setYear: PropTypes.func.isRequired
-}
+};
